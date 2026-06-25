06d943c3f9009aea3ecab77ffdc2e5e525477f22
Gabriel Broussard Korr <mantis9877@gmail.com>
2026-06-25 00:16:33 -0700

    network3d test

--- R/sim.R
+++ R/sim.R
@@ -2,9 +2,9 @@
 
 
 init = list(
-	length = 4, #length of region in which to generate particles
-	width = 4,
-	height = 1e-8,
+	length = 1, #length of region in which to generate particles
+	width = 1,
+	height = 1,
 
 	density = 0.1, #particle density; can also be function. (0,1].
 	init_seed = 1 #set.seed() for particle distribution
@@ -12,9 +12,9 @@ init = list(
 
 rules = list(
 	structure_seed = 1, #set.seed() for structure generation; preferred over init_seed
-	dimension = 2, #links per node; does the model use links, triangles, or tetrahedra?
+	dimension = 3, #links per node; does the model use links, triangles, or tetrahedra?
 
-	link_range = 0.18,
+	link_range = 0.2,
 	mobility = 0.04,
 	contraction = 1.0,
 	cohesion = 0,
@@ -92,8 +92,6 @@ Nodes = function(rules){
 
 enchunk = function(pos) paste0(pos, collapse=',')
 Chunk = function(pts,size){ #fuzz: include some points in multiple if they sit on the border, in case they drift
-	size=1
-	warning('chunking nonfunctional atm')
 	chunks = list(size=size) #3D list
 	for (p in 1:nrow(pts)){
 		chunkpos = enchunk(floor(pts[p,1:3]/size))
@@ -109,12 +107,13 @@ Sim = function(init,rules){
 	sim$nodes = Nodes(rules)
 	set.seed(rules$seed)
 
-	sim$chunks = Chunk(sim$points, rules$link_range * 2)
-
 	#first link
 	sim$points = rbind(c(init$length/2,init$width/2,init$height/2,1),sim$points) #point in the center
 	sim$nodes[1,] = c(rep(1,rules$dimension),rep(0,3-rules$dimension),1,1,0,0,0,0,1,sim$points[1,1:3]) #node connected to only that point
 	sim$nid = 2 #first open spot in sim$nodes
+
+
+	sim$chunks = Chunk(sim$points, rules$link_range * 18)
 
 	sim
 }
@@ -128,6 +127,8 @@ grow = function(sim, nid){
 	chunkpos = floor(pos / sim$chunks$size)
 	subchunkpos = sign((pos / sim$chunks$size - chunkpos) - 0.5)
 
+	#chunking just isn't worth it lmfao
+	#vectorization calcs are cheaper than all this shuffling
 	#get all point pids from a 2x2x2 cube of chunks around the node
 	close_pids = c(
 		sim$chunks[[enchunk(chunkpos)]],
@@ -155,7 +156,7 @@ grow = function(sim, nid){
 		for (p in 1:sim$rules$dimension){ #add number of nodes equal to dimension
 			sim$nodes[nid, 7 + p] = sim$nid #add child
 			pts = c(pid, node[(1:3)[-p]]) #exclude one of the old points
-			sim$nodes[sim$nid,] = c(pts, 10, node[5] + 1, nid, 0, 0, 0, 1, 0, 0, 0) #pos will get updated during contraction #ADD MOBILITY HERE
+			sim$nodes[sim$nid,] = c(pts, 100, node[5] + 1, nid, 0, 0, 0, 1, 0, 0, 0) #pos will get updated during contraction #ADD MOBILITY HERE
 			sim$nid = sim$nid + 1
 		}
 	}
@@ -184,7 +185,7 @@ contract = function(sim){
 
 	glob_avg = cbind(avg_pos,avg_pos,avg_pos) #duplicate to overlay point_glob
 
-	glob_velocity = (glob_avg - glob_points) * 0.01 #CONTRACTION
+	glob_velocity = (glob_avg - glob_points) * 0.0001 #CONTRACTION
 
 	for (n in 1:nrow(glob_points)){
 		for (p in 1:3){
@@ -228,10 +229,21 @@ view_network = function(sim){
 	}
 }
 
+network3d = function(sim){
+	real_nids = which(sim$nodes[,1] > 0)
+	n_real = length(real_nids)
+	plot3d(NULL,xlim=c(0,sim$init$length),ylim=c(0,sim$init$width))
+	for (n in 1:n_real){
+		node = sim$nodes[n,]
+		pnode = sim$nodes[node[6],]
+		lines3d(c(node[11],pnode[11]),c(node[12],pnode[12]),c(node[13],pnode[13]))
+	}
+}
+
 sim=Sim(init,rules)
-for(i in 1:1000){
+if (1) for(i in 1:1000){
 	cat('\r',i,'     ')
 	sim=tick(sim)
 }
-view_network(sim)
+network3d(sim)
 
